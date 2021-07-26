f6b36a2089be0bb66186330f551bb65d888e3a7d
ricardo <ricardolopez28@gmail.com>
2021-07-26 18:33:47 -0500

    Creando componente Button para el detalle, y rutas programaticas

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -21,29 +21,52 @@
           <img class="w-5 h-5" :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"  :alt="a.name">
         </td>
         <td> #{{a.rank}}</td>
-        <td> {{a.name}} </td>
+        <td>
+          <router-link
+          class="hover:underline text-green-700"
+          :to="{name: 'coin-detail', params: {id: a.id} }"
+          > 
+          {{a.name}} 
+          </router-link>
+          <small class="ml-1 text-gray-500">{{ a.symbol }}</small>
+        </td>
         <td> {{a.priceUsd | dollar}} </td>
         <td> {{ a.marketCapUsd | dollar}}</td>
         <td 
           :class="a.changePercent24Hr.includes('-')? 'text-red-600': 'text-green-600'"> 
           {{a.changePercent24Hr | percent}}
         </td>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <px-button @custom-click="goToCoin(a.id)">
+            <span>Detalle</span>
+          </px-button>
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
+import PxButton from '@/components/PxButton'
+
 export default {
   name: "PxAssetsTable",
+
+  components: {PxButton},
 
   props: {
     assets: {
       type: Array,
       default: () => []
     }
+  },
+
+  methods:{
+    goToCoin(id){
+      this.$router.push({name: 'coin-detail', params:{id} })
+    }
   }
+
 };
 </script>
 
