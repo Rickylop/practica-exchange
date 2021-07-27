6ac248fda72fbf6791ff188c3dead610f97c2535
ricardo <ricardolopez28@gmail.com>
2021-07-27 13:30:33 -0500

    Componentes de terceros

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <px-assets-table v-bind:assets="assets" />
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assets-table v-if="!isLoading" v-bind:assets="assets" />
   </div>
 </template>
 
@@ -14,16 +15,16 @@ export default {
 
   data() {
       return {
-          assets: []
+        isLoading: false,
+        assets: []
       }
   },
 
   created(){
-      api.getAssets().then(assets => {
-        this.assets = assets
-        
-        console.log('arrayy: ', this.assets);
-        })
+    this.isLoading = true;
+    api.getAssets().then(assets => {
+      this.assets = assets      
+   }).finally(() =>this.isLoading = false);
   }
 };
 </script>
