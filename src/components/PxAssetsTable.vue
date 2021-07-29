f65a9250e2d684fb160b32d03618031b5a04e94a
ricardo <ricardolopez28@gmail.com>
2021-07-29 12:13:24 -0500

    Adicioando filtro al listado de la tabla

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -10,12 +10,20 @@
         <th>Precio</th>
         <th>Cap. de Mercado</th>
         <th>Variación 24hs</th>
-        <td class="hidden sm:block"></td>
+        <td class="hidden sm:block">
+          <input
+            class="bg-gray-100 focus:outline-none border-b border-gray-400 py-2 px-4 block w-full appearance-none leading-normal"
+            id="filter"
+            placeholder="Buscar..."
+            type="text"
+            v-model="filter"
+          />
+        </td>
       </tr>
     </thead>
     <tbody>
       <tr 
-      v-for="a in assets" :key="a.id"
+      v-for="a in filteredAssets" :key="a.id"
       class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100">
         <td>
           <img class="w-5 h-5" :src="`https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`"  :alt="a.name">
@@ -54,10 +62,28 @@ export default {
 
   components: {PxButton},
 
+  data() {
+    return {
+      filter: ''  
+    }
+  },
   props: {
     assets: {
       type: Array,
       default: () => []
+    }
+  },
+
+  computed: {
+    filteredAssets(){
+      if (!this.filter) {
+        return this.assets;
+      }
+
+      return this.assets.filter(a => 
+        a.symbol.toLowerCase().includes(this.filter.toLowerCase()) ||
+        a.name.toLowerCase().includes(this.filter.toLowerCase())
+      )
     }
   },
 
